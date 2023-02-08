6f6bbccbf3e2590478ea9c290fca310409b78e9d
Semyon <sema.kutin@mail.ru>
2023-02-08 10:50:46 +0300

    Initial commit

---
+++ tests/first_test.py
@@ -0,0 +1,23 @@
+import pytest
+from calculator import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_calculation_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    #Напишите по одному позитивному тесту для каждого метода калькулятора.
+
+    def test_division_caclulate_correctly(self):
+        assert self.calc.division(self, 18, 6) == 3 #Деление тест
+
+    def test_subtraction_caclulate_correctly(self):
+        assert self.calc.subtraction(self, 12, 5) == 7  # Вычитание тест
+
+    def test_adding_caclulate_correctly(self):
+        assert self.calc.adding(self, 3, 2) == 5  # Сложение тест
